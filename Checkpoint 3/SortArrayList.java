f14997e1aa5526bb7c7f420ca215ea66f07ddb8c
Phutawa <phutawanmuangma@gmail.com>
2025-02-27 11:07:03 +0700

    Add files via upload

---
+++ Checkpoint 3/SortArrayList.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+;
+public class SortArrayList {
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+        Collections.sort(list);
+    }
+    public static void main(String[] args) {
+        ArrayList<String> word = new ArrayList<>();
+        word.add("Peach"); 
+        word.add("Apple"); 
+        word.add("Orange"); 
+        word.add("Banana"); 
+        word.add("Grape"); 
+
+        System.out.println("Original list: "+word);
+        sort(word);
+        System.out.println("Sorted list: "+word);
+    }
+    
+}
